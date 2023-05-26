b4fa1b635b4eeef4a80859f049df6dacbc1bc28a
Dmitriifd <idimka9@gmail.com>
2023-05-26 20:14:51 +0500

    section order mobile

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,34 @@
 import { isMobile } from './functions.js';
 // Подключение списка активных модулей
 import { flsModules } from './modules.js';
+
+Array.prototype.forEach.call(
+  document.querySelectorAll('.file-upload__button'),
+  function (button) {
+    const hiddenInput = button.parentElement.querySelector(
+      '.file-upload__input'
+    );
+    const label = button.parentElement.querySelector('.file-upload__label');
+    const defaultLabelText = '';
+
+    // Set default text for label
+    label.textContent = defaultLabelText;
+    label.title = defaultLabelText;
+
+    button.addEventListener('click', function () {
+      hiddenInput.click();
+    });
+
+    hiddenInput.addEventListener('change', function () {
+      const filenameList = Array.prototype.map.call(
+        hiddenInput.files,
+        function (file) {
+          return file.name;
+        }
+      );
+
+      label.textContent = filenameList.join(', ') || defaultLabelText;
+      label.title = label.textContent;
+    });
+  }
+);
